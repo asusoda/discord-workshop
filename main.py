0f94e151de9251ddd603d7f3955d0225c249273a
rtwoo <rtwoo@asu.edu>
2024-02-21 09:48:55 -0700

    Added slash command - Added GUILD_ID env var

--- main.py
+++ main.py
@@ -6,37 +6,29 @@ import discord
 from discord.ext import commands
 client = commands.Bot(
     intents=discord.Intents.all(),
-    command_prefix="%"
+    command_prefix='!'
 )
-
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    for guild in client.guilds:
-        print(guild.name)
-        for category in guild.categories:
-            print(f"|-{category.name}")
-            for channel in category.channels:
-                print(f" |-{channel.name}")
-                perms = channel.permissions_for(guild.me)
-                if perms.send_messages:
-                    print(f"  |-I can send messages in {channel.name}")
-                if perms.view_channel:
-                    print(f"  |-I can view {channel.name}")
-                if perms.read_messages:
-                    print(f"  |-I can read messages in {channel.name}")
-                if perms.manage_messages:
-                    print(f"  |-I can manage messages in {channel.name}")
-                if perms.manage_channels:
-                    print(f"  |-I can manage channels in {channel.name}")
-                if perms.administrator:
-                    print(f"  |-I am an admin in {channel.name}")
-                    
 
+    # register application command tree with Discord 
+    await client.tree.sync(guild=discord.Object(id=int(os.getenv('GUILD_ID')))) # register for only specific guild to decrease delay
+
+# EXAMPLE: OLD PREFIX COMMAND
 @client.command()
-async def test(ctx):
-    print(ctx.guild.name)
+async def prefixtest(ctx: commands.Context):
+    await ctx.channel.send(f'Hi {ctx.author.name}! Welcome to {ctx.guild.name}.')
+
+# EXAMPLE: SLASH COMMAND
+@client.tree.command(
+    name='slashtest',
+    description='A test slash command',
+    guild=discord.Object(id=int(os.getenv('GUILD_ID'))) # register for only specific guild to decrease delay
+)
+async def slashtest(interaction: discord.interactions.Interaction):
+    await interaction.response.send_message(f'Hi {interaction.user.name}! Welcome to {interaction.guild.name}.')
 
 client.run(os.getenv('DISCORD_BOT_TOKEN'))
 
